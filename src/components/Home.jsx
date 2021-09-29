3ba0d12ff31588bed36e7ef19836970589b64d30
Waz0x <remy.noulin@epitech.eu>
2021-09-29 16:15:56 +0200

    MOD | bruh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
-import React, {useRef, useState} from "react";
-import Flippy, {BackSide, FrontSide} from "react-flippy";
+import React, {useRef, useState} from 'react';
+import Flippy, {BackSide, FrontSide} from 'react-flippy';
 import 'reactjs-popup/dist/index.css';
 import data from '../parrains/parrains.json'
-import {Container, Row, Col} from "react-grid-system";
+import {Container, Row, Col} from 'react-grid-system';
 import './home.css'
+import '../lib/dbHandler'
 
 function choose(props) {
     if (parseInt(localStorage.getItem("number")) >= parseInt(process.env.REACT_APP_MAX_PAR)) {
@@ -13,9 +14,35 @@ function choose(props) {
     localStorage.setItem("parrain" + localStorage.getItem("number"), props.id.toString())
 }
 
+function remove(props) {
+    if (parseInt(localStorage.getItem("number")) === 0) {
+        return alert("Tu as 0 parrains tu ne peux pas en retirer plus")
+    }
+    localStorage.removeItem("parrain" + localStorage.getItem("number"))
+    localStorage.setItem("number", (parseInt(localStorage.getItem("number")) - 1).toString())
+}
+
+function isEnabled(id) {
+    let nbr = parseInt(localStorage.getItem("number"));
+
+    for (let i = 0; i < nbr; ++i) {
+        if (parseInt(localStorage.getItem("parrain" + i.toString())) === id) {
+            return true
+        }
+    }
+    return false
+}
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
+
 function Sample(props) {
     const ref = useRef();
     const par = props.par
+    const enabled = isEnabled(par.id)
+    const forceUpdate = useForceUpdate();
     return (
         <Flippy
             flipOnHover={true}
@@ -28,7 +55,7 @@ function Sample(props) {
                 <img
                     src={process.env.PUBLIC_URL + `/images/${par.img}`}
                     style={{ maxWidth: '100%', maxHeight: '100%' }}
-                    alt={"Image"}
+                    alt={"Error, contact an admin if the problem persist"}
                 />
             </FrontSide>
             <BackSide style={{ backgroundColor: '#175852'}}>
@@ -36,18 +63,28 @@ function Sample(props) {
                 - Aime {par.like}<br/>
                 - Aime pas {par.nlike}<br/>
                 - Passions {par.passion}<br/>
-                <button onClick={() => { choose(par); }}>Choisir</button>
-            </BackSide>
+                {
+                    (!enabled ?
+                    <button onMouseDown={function (event) {
+                        forceUpdate();
+                        choose(par)
+                    }}>Choisir</button>
+                :
+                    <button class="disabled" onMouseDown={function (event) {
+                        forceUpdate()
+                        remove()
+                    }}>Supprimer</button>)
+                }
+                </BackSide>
         </Flippy>
     )
 }
 
 function Home() {
     let len = Object.keys(data.parrains).length;
-    let size = Math.trunc(Math.sqrt(len))
     const [parrains, setParrains] = useState(data.parrains)
     var rows = [];
-    for (var i = 0; i < len; i++) {
+    for (var i = 0; i < len; ++i) {
         console.log(parrains[i])
         rows.push(<Col><Sample par={parrains[i]}/></Col>)
     }
