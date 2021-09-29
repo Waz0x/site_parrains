391b439a94007ed4ea0b244dfc6bf79f8ddecab1
Waz0x <remy.noulin@epitech.eu>
2021-09-29 11:30:09 +0200

    MOD | add json config file and image folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import Flippy, {BackSide, FrontSide} from "react-flippy";
-import MN from "../Mage_Noir.png";
 import 'reactjs-popup/dist/index.css';
+import data from '../parrains/parrains.json'
+import {Container, Row, Col} from "react-grid-system";
 
-function Sample() {
+function choose(props) {
+    alert(props.name)
+}
+
+function Sample(props) {
     const ref = useRef();
+    const par = props.par
     return (
         <Flippy
             flipOnHover={false}
@@ -15,30 +21,41 @@ function Sample() {
         >
             <FrontSide style={{ backgroundColor: '#41669d'}} >
                 <img
-                    src={MN}
+                    src={process.env.PUBLIC_URL + `/images/${par.img}`}
                     style={{ maxWidth: '100%', maxHeight: '100%' }}
-                    alt={"Mage Noir"}
+                    alt={"Image"}
                 />
             </FrontSide>
             <BackSide style={{ backgroundColor: '#175852'}}>
-                - Description <br/>
-                - Aime <br/>
-                - Aime pas <br/>
-                - Passions <br/>
-                <button onClick={() => { ref.current.toggle(); }}>Choose</button>
+                - Description: {par.desc} <br/>
+                - Aime {par.like}<br/>
+                - Aime pas {par.nlike}<br/>
+                - Passions {par.passion}<br/>
+                <button onClick={() => { choose(par); }}>Choose</button>
             </BackSide>
         </Flippy>
     )
 }
 
 function Home() {
+    let len = Object.keys(data.parrains).length;
+    let size = Math.trunc(Math.sqrt(len))
+    const [parrains, setParrains] = useState(data.parrains)
+    var rows = [];
+    for (var i = 0; i < len; i++) {
+        console.log(parrains[i])
+        rows.push(<Col><Sample par={parrains[i]}/></Col>)
+    }
+
     return (
         <div className="home">
             <div class="container">
                 <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <Sample></Sample>
-                    </div>
+                        <Container fluid>
+                            <Row>
+                                {rows}
+                            </Row>
+                        </Container>
                 </div>
             </div>
         </div>
