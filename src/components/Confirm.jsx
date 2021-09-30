01302be81e531db0b03511d51feccf39ace13ec4
Waz0x <remy.noulin@epitech.eu>
2021-09-30 18:56:11 +0200

    MOD | last version

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -2,25 +2,28 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
-import webhook from "webhook-discord";
 
 export default function Login() {
     const [email, setEmail] = useState("");
-    const wh = new webhook.Webhook(process.env.REACT_APP_WEBHOOK_URL)
 
     function validateForm() {
         return email.length > 0;
     }
 
     async function handleSubmit(event) {
-        const par = localStorage.getItem("parrains").toString()
+        const par = email + ":" + localStorage.getItem("parrains").toString()
         event.preventDefault();
         if (par === "") {
             return alert("Vous ne pouvez pas valider sans choisir de parrain")
         }
         if ((email.split("@")[1] === "epitech.eu") || (email.split("@")[1] === "epitech.digital")) {
-            alert("good")
-            wh.info("New response", "Mail: " + email + "\nId: " + par.split(';').join(' '))
+            fetch("http://51.91.255.204:8080/?email=" + par).then(function (response) {
+                if (response.status !== 200) {
+                    alert("Something went wrong please contact admin, error:" + response.status.toString())
+                } else {
+                    alert("Merci de votre participation.\nvous allez etre prevenus dans les plus brefs délais par vos parrains")
+                }
+            })
         } else {
             alert("Error:\nPlease use your epitech-mail adress (@epitech.eu or @epitech.digital)");
         }
