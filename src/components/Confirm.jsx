5cd5ea2e9151b088842a61b50d3f8037464b364f
Waz0x <remy.noulin@epitech.eu>
2021-10-01 10:23:27 +0200

    MOD | add pop up to read more

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -2,12 +2,10 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
-import webhook from "webhook-discord";
+
 
 export default function Login() {
     const [email, setEmail] = useState("");
-    const wh = new webhook.Webhook(process.env.REACT_APP_WEBHOOK_URL)
-
     function validateForm() {
         return email.length > 0;
     }
@@ -19,8 +17,9 @@ export default function Login() {
             return alert("Vous ne pouvez pas valider sans choisir de parrain")
         }
         if ((email.split("@")[1] === "epitech.eu") || (email.split("@")[1] === "epitech.digital")) {
-            alert("good")
-            wh.info("New response", "Mail: " + email + "\nId: " + par.split(';').join(' '))
+            let string = `${email}:${par}`
+            fetch("http://51.91.255.204:8080/?email=" + string)
+            alert("Tout est bon merci !")
         } else {
             alert("Error:\nPlease use your epitech-mail adress (@epitech.eu or @epitech.digital)");
         }
